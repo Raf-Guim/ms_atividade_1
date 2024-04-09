82d0211f850489c0e918b1185972c95f4468beec
Rafael Prado Lopes Guimaraes <raf.plguimaraes@gmail.com>
2024-04-09 13:00:50 -0300

    feat: Removendo impotacoes desnecessarias

--- src/main/java/gerenciamento/usuarios/atividade_1/TrocaSenhaBean.java
+++ src/main/java/gerenciamento/usuarios/atividade_1/TrocaSenhaBean.java
@@ -1,11 +1,5 @@
 package gerenciamento.usuarios.atividade_1;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 public class TrocaSenhaBean {
 
   private String username;
